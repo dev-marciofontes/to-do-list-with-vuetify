b4651974d368e1760fef07d4d3c86d6ccb874eae
Márcio Fontes <contato@marciofontes.com>
2024-04-24 15:42:50 -0300

    Enviando projeto

---
+++ src/components/Alert.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="alert-notification">
+        <v-alert :text="alertStore.text" :type="alertStore.type" closable
+            class="animate__animated animate__pulse"></v-alert>
+    </div>
+</template>
+
+<script setup>
+import 'animate.css';
+import { } from '@/store/alert';
+const alertStore = useAlertStore();
+</script>
+
+<style scoped>
+.alert-notification {
+    position: fixed;
+    z-index: 1;
+    bottom: 50px;
+    width: 100%;
+    padding: 0 35% 0 35%;
+}
+</style>
